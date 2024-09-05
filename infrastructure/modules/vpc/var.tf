c68d05924537b9f25d1f34e5e170646667a2368a
Ashutosh <ashutosh.pathania@growthnatives.com>
2024-09-06 00:52:51 +0530

    changes-infra

--- infrastructure/modules/vpc/var.tf
+++ infrastructure/modules/vpc/var.tf
@@ -2,20 +2,30 @@
 
 variable "name" {
   description = "Name of the VPC"
-  type        = string
+  type        = "eks-infa-vpc"
 }
 
 variable "cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
+
 
 variable "public_subnets" {
   description = "List of public subnet CIDR blocks"
   type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
 }
 
 variable "private_subnets" {
   description = "List of private subnet CIDR blocks"
   type        = list(string)
+  default = [
+    "10.0.3.0/24",
+    "10.0.4.0/24"
+  ]
 }
